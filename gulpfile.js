2b43e9050df65fc5e35cd4efb5c8aa0309a2ed75
Marco Pracucci <marco.pracucci@spreaker.com>
2014-12-16 13:26:20 +0100

    Minimized lib

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp   = require('gulp'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
 
 gulp.task('release', function() {
     gulp.src('./src/*.js')
         .pipe(concat('spreaker.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('./lib/'));
 });
