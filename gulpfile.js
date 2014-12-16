49985954ce85a1794a1215fa778587d8a4fbb607
Marco Pracucci <marco.pracucci@spreaker.com>
2014-12-16 14:51:59 +0100

    Added spreaker.js.gz version

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp   = require('gulp'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    gzip   = require('gulp-gzip');
 
 gulp.task('release', function() {
     gulp.src('./src/*.js')
         .pipe(concat('spreaker.js'))
         .pipe(uglify())
+        .pipe(gzip())
         .pipe(gulp.dest('./lib/'));
 });
