28bc9432121ce3cb4ca774f0040ca979e4a01bb2
Marco Pracucci <marco.pracucci@spreaker.com>
2014-12-16 15:01:38 +0100

    Added linter

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp   = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    gzip   = require('gulp-gzip');
+    gzip   = require('gulp-gzip'),
+    jshint = require('gulp-jshint');
+
+
+gulp.task('lint', function() {
+    gulp.src('./src/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
 
 gulp.task('release', function() {
     gulp.src('./src/*.js')
+        // Break on lint errors
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'))
+
+        // Concat + minify + gzip
         .pipe(concat('spreaker.js'))
         .pipe(uglify())
         .pipe(gzip())
