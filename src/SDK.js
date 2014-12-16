877325c2e6ca2b8e5e462fd3af51605379ba0dd4
Marco Pracucci <marco.pracucci@spreaker.com>
2014-12-16 16:59:30 +0100

    Simplified the async version

--- src/SDK.js
+++ src/SDK.js
@@ -2,13 +2,7 @@ var Auth = require('./Auth.js');
 
 window.SP = Auth;
 
-// Execute queued commands (async version)
-if (_sp) {
-
-    for (var i = 0; i < _sp.length; i++) {
-        var fn   = _sp[i][0];
-        var args = _sp[i].splice(1);
-
-        window.SP[fn].apply(window.SP, args);
-    }
+// Notify that the lib is ready
+if (window._spOnReady) {
+    window._spOnReady();
 }
